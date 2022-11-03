920e589def532ca00bcdeb59944c0c0d8de90910
Venkata Sravan Telu <s555053@nwmissouri.edu>
2022-11-02 22:30:21 -0500

    Update car.js

--- routes/car.js
+++ routes/car.js
@@ -1,9 +1,10 @@
+module.exports = router;
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('car', { title: 'Venkata Sravan Telu Search Results' });
 });
 
 module.exports = router;
