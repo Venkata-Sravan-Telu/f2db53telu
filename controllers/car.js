8d87692ab559d80d31787050786db6d1ea25c0ed
Venkata Sravan Telu <s555053@nwmissouri.edu>
2022-11-16 17:42:10 -0600

    done

--- controllers/car.js
+++ controllers/car.js
@@ -35,6 +35,20 @@ exports.car_create_post = async function(req, res) {
 exports.car_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: car delete DELETE ' + req.params.id); 
 }; 
+
+ // Handle building the view for creating a car. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.car_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('carcreate', { title: 'Car Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
  
  
 
@@ -110,4 +124,59 @@ failed`);
 
     }
 
+}; 
+
+// Handle Costume delete on DELETE. 
+exports.car_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await car.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+// Handle a show one view with id specified by query 
+exports.car_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await car.findById( req.query.id) 
+        res.render('cardetail',  
+{ title: 'car Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a car. 
+// query provides the id 
+exports.car_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await car.findById(req.query.id) 
+        res.render('carupdate', { title: 'Car Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle a delete one view with id from query 
+exports.car_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await car.findById(req.query.id) 
+        res.render('cardelete', { title: 'Car Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/car.js
+++ routes/car.js
@@ -1,4 +1,5 @@
 module.exports = router;
+const car_controlers = require('../controllers/car');
 var express = require('express');
 var router = express.Router();
 
@@ -6,5 +7,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('car', { title: 'Venkata Sravan Telu Search Results' });
 });
-
+/* GET detail car page */ 
+router.get('/detail', car_controlers.car_view_one_Page); 
+/* GET create car page */ 
+router.get('/create', car_controlers.car_create_Page); 
+/* GET create update page */ 
+router.get('/update', car_controlers.car_update_Page); 
+/* GET delete car page */ 
+router.get('/delete', car_controlers.car_delete_Page); 
 module.exports = router;
