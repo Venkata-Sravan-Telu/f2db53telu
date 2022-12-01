58528962e7e255219ef2c2cb262fd38a6fba42cd
Venkata Sravan Telu <s555053@nwmissouri.edu>
2022-11-30 19:12:26 -0600

    add

--- models/car.js
+++ models/car.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const carSchema = mongoose.Schema({ 
- car_name: String, 
- car_price: Number, 
- car_model: Number 
+    car_name: {type: String,required: [true, 'Car Name cannot be empty']},
+    car_price: {type: Number,required: [true, ' car price cannot be empty']},
+    car_model: {type: Number,required: [true, 'car model cannot be empty']}
 }) 
  
 module.exports = mongoose.model("car", carSchema) 
--- routes/car.js
+++ routes/car.js
@@ -2,17 +2,25 @@ module.exports = router;
 const car_controlers = require('../controllers/car');
 var express = require('express');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('car', { title: 'Venkata Sravan Telu Search Results' });
-});
+router.get('/', car_controlers.car_view_all_Page );
 /* GET detail car page */ 
 router.get('/detail', car_controlers.car_view_one_Page); 
 /* GET create car page */ 
-router.get('/create', car_controlers.car_create_Page); 
+router.get('/create', secured, car_controlers.car_create_Page); 
 /* GET create update page */ 
-router.get('/update', car_controlers.car_update_Page); 
+router.get('/update', secured, car_controlers.car_update_Page); 
 /* GET delete car page */ 
-router.get('/delete', car_controlers.car_delete_Page); 
+router.get('/delete', secured,  car_controlers.car_delete_Page); 
 module.exports = router;
